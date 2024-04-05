75565154b242465257597c0cc2b8036076c3b760
wilmer <wilmer.hernandez.ext@itau.co>
2024-04-05 16:23:21 -0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as csurf from 'csurf';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Configurar csurf middleware
+  app.use(csurf(), cors<cors.CorsRequest>({ origin: ['https://backpost-v1jb.onrender.com', 'https://studio.apollographql.com'] }),);
+
   await app.listen(3000);
 }
 bootstrap();
